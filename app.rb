2f17f99686c1791796e0aa96360dfe5363c0ed59
aisha dorsett <aishadorsett23@gmail.com>
2021-10-04 21:25:33 +0100

    sinatra - the web week 5

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+# app.rb is used for routing and views is used for our html code
+
+class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
+
+  get '/' do
+    "hello!"
+  end
+
+  get '/secret' do
+    "this is a secret page"
+  end
+
+  get '/aisha' do
+    "this is Aisha's page!"
+  end
+
+  get '/cat' do
+    @name = ["Amigo", "Misty", "Almond"].sample
+    erb(:index)
+  end
